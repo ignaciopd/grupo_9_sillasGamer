5bf31a74c15a505d9a226abeb8b5f5bba9b01a2f
diegoleonc51 <diegoleonc51@gmail.com>
2021-08-25 20:13:09 -0400

    changes in app.js

--- app.js
+++ app.js
@@ -23,5 +23,8 @@ app.get("/sign-in", (req, res) => {
   
 app.get("/product-detail", (req, res) => {
     res.sendFile(path.join(__dirname, "./view/productDetail.html"));
+
+app.get("/cart", (req, res) => {
+      res.sendFile(path.join(__dirname, "./view/cart.html"));
   });  
 app.listen(puerto, () => console.log(`escuchando en el puerto ${puerto}`));
